1e2c88d4f657c4d55a14cd94bab9344684f4804a
AkASH jain <aj9882854@gmail.com>
2025-05-14 18:54:33 +0530

    push multer cloud errro

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,15 +1,37 @@
+// import multer from "multer";
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, "./public/temp")
+//     },
+//     filename: function (req, file, cb) {
+      
+//       cb(null, file.originalname)
+//     }
+//   })
+  
+// export const upload = multer({ 
+//     storage, 
+// })
+
 import multer from "multer";
+import fs from "fs";
+import path from "path";
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "./public/temp")
-    },
-    filename: function (req, file, cb) {
-      
-      cb(null, file.originalname)
+  destination: function (req, file, cb) {
+    const dir = path.join(__dirname, "../public/temp");
+
+    // Create directory if it doesn't exist
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
     }
-  })
-  
-export const upload = multer({ 
-    storage, 
-})
+
+    cb(null, dir);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+export const upload = multer({ storage });
